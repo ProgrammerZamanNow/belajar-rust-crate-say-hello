cfd7e961e70d16f60b986ee990061689d9fb631e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-05-11 23:42:15 +0700

    upgrade to version 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ pub fn say_hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+pub fn say_hello_to_everyone() -> String {
+    "Hello, everyone!".to_string()
+}
+
 pub fn say_goodbye(name: &str) -> String {
     format!("Goodbye, {}!", name)
+}
+
+pub fn say_goodbye_to_everyone() -> String {
+    "Goodbye, everyone!".to_string()
 }
