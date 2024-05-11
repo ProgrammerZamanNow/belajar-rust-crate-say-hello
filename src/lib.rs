8f48d64eb2520050633327d3ace189be3582b12c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-05-11 23:53:51 +0700

    upgrade to version 0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
+#[cfg(feature = "hello")]
 pub fn say_hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[cfg(feature = "hello")]
 pub fn say_hello_to_everyone() -> String {
     "Hello, everyone!".to_string()
 }
 
+#[cfg(feature = "bye")]
 pub fn say_goodbye(name: &str) -> String {
     format!("Goodbye, {}!", name)
 }
 
+#[cfg(feature = "bye")]
 pub fn say_goodbye_to_everyone() -> String {
     "Goodbye, everyone!".to_string()
 }
